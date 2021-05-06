4414932e20738ed97acf1b54da1548297fb62c5d
TweekerGit <programmeracc38@gmail.com>
2021-05-06 13:52:55 +0300

    Service

---
+++ src/main/java/org/obrii/dp2021/RestJPA/repository/StudentRepository.java
@@ -0,0 +1,13 @@
+package org.obrii.dp2021.RestJPA.repository;
+
+import java.util.List;
+import org.obrii.dp2021.RestJPA.entity.Student;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+
+@RepositoryRestResource(collectionResourceRel="student", path="student")
+public interface StudentRepository extends PagingAndSortingRepository<Student,Long>{
+    List<Student> findByName(@Param("name") String name);
+}
